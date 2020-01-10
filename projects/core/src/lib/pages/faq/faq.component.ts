338e0d3f28110e5ae9c9b862634fec60f0da1825
Mohamed Gontara <gontaramohamed1@gmail.com>
2020-01-10 18:43:14 +0100

    Add commun pages

---
+++ projects/core/src/lib/pages/faq/faq.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-faq',
+  templateUrl: './faq.component.html'
+})
+export class FaqComponent {
+  constructor() {}
+}
--- projects/download/src/app/download/download-routing.module.ts
+++ projects/download/src/app/download/download-routing.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-import { IndexComponent } from '../../../../core/src/public-api';
+import { IndexComponent, FaqComponent, MlComponent, CguComponent } from '../../../../core/src/public-api';
 import { DownloadSectionComponent } from './download-section/download-section.component';
 
 const routes: Routes = [
@@ -9,6 +9,18 @@ const routes: Routes = [
     path: '',
     component: IndexComponent,
     children: [{ path: '', component: DownloadSectionComponent }]
+  },
+  {
+    path: 'faq',
+    component: FaqComponent
+  },
+  {
+    path: 'ml',
+    component: MlComponent
+  },
+  {
+    path: 'cgu',
+    component: CguComponent
   }
 ];
 
