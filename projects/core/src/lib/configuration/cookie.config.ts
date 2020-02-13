ab65eb33cc5eb2b9ffc01dd472a3dbc45ca22ed6
Mohamed Gontara <gontaramohamed1@gmail.com>
2020-02-13 15:44:49 +0100

    (Fix) anomalie 13/02/2020

--- projects/core/src/lib/configuration/cookie.config.ts
+++ projects/core/src/lib/configuration/cookie.config.ts
@@ -1,5 +1,7 @@
 export const COOKIES_CONSTANTS = {
   CONSENTED: 'HAS_CONSENTED',
   EXPIRED: 396,
-  PATH: '/'
+  PATH: '/',
+  DOMAINE: window.location.hostname,
+  SECURE: window.location.protocol !== 'http:'
 };
