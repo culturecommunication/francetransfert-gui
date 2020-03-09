23c80f0c9b82609bffd3ca9a5e30c99b911fd0bb
TAHTAH Hussein <EXT_HTA37@open-groupe.com>
2020-03-09 16:43:43 +0100

    committing changes to message error

--- projects/core/src/lib/configuration/errors-messages.config.ts
+++ projects/core/src/lib/configuration/errors-messages.config.ts
@@ -2,7 +2,7 @@ export const MSG_ERR = {
   MSG_ERR_00: 'Vous avez dépassé la limite de 20 GO',
   MSG_ERR_01: 'Les deux mots de passe ne sont pas identiques. Veuillez les ressaisir.',
   MSG_ERR_02:
-    'Le service en test « FranceTransfert » est réservé aux échanges de fichiers/dossiers volumineux avec l’administration d’état (à destination d’adresses de courriels se terminant par @xxx.gouv.fr).',
+    'Le service en béta test « FranceTransfert » est réservé aux échanges de fichiers/dossiers volumineux avec l’administration d’État (à destination d’adresses de courriels se terminant par @xxx.gouv.fr) @xxx.gouv@xxx.gouv.',
   MSG_ERR_03: 'Il est nécessaire de télécharger au moins un fichier ou un dossier pour démarrer le transfert.',
   MSG_ERR_04: 'Il semble qu’au moins une des adresses de courriels soit erronée. Merci de vérifier.',
   MSG_ERR_06: 'Merci de renseigner au moins un destinataire pour votre transfert.',
