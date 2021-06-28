09231e6c4d24e887c81c018378b3d939baccb59b
Julien Aubert <j.aubert@actongroup.com>
2021-06-28 16:33:42 +0000

    Update upload.service.ts

--- projects/upload/src/app/upload/services/upload.service.ts
+++ projects/upload/src/app/upload/services/upload.service.ts
@@ -42,7 +42,7 @@ export class UploadService {
       confirmedSenderId: '',
       senderEmail: body.senderMail,
       recipientEmails: body.emails,
-      password: body.password,
+      password: '',
       message: body.message,
       rootFiles: trMapping.files,
       rootDirs: trMapping.dirs
