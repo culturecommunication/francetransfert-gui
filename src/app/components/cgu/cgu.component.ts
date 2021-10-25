cb062612523d819d0682ccff4633963e7e45aa69
Romain Lalanne <r.lalanne@actongroup.com>
2021-10-25 12:23:13 +0200

    feat: ajout liens rapides rgaa #38893

--- src/app/components/cgu/cgu.component.ts
+++ src/app/components/cgu/cgu.component.ts
@@ -1,17 +1,67 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'ft-cgu',
   templateUrl: './cgu.component.html',
   styleUrls: ['./cgu.component.scss']
 })
-export class CguComponent implements OnInit {
+export class CguComponent implements OnInit, AfterViewInit {
 
-  constructor(private titleService: Title) { }
+  @ViewChild('cgu') private cguFragment: ElementRef;
+  @ViewChild('presentationduservice') private presentationduserviceFragment: ElementRef;
+  @ViewChild('modalites') private modalitesFragment: ElementRef;
+  @ViewChild('traitementdesdonnees') private traitementdesdonneesFragment: ElementRef;
+  @ViewChild('conservationdesdonnees') private conservationdesdonneesFragment: ElementRef;
+  @ViewChild('engagementsetresponsabilite') private engagementsetresponsabiliteFragment: ElementRef;
+  @ViewChild('qualiteduservice') private qualiteduserviceFragment: ElementRef;
+  @ViewChild('securite') private securiteFragment: ElementRef;
+  @ViewChild('validite') private validiteFragment: ElementRef;
+
+  constructor(private titleService: Title,
+    private router: Router) { }
 
   ngOnInit(): void {
     this.titleService.setTitle('France transfert - CGU');
   }
 
+  ngAfterViewInit(): void {
+    const tree = this.router.parseUrl(this.router.url);
+    if (tree.fragment) {
+      this.scrollTo(tree.fragment);
+    }
+  }
+
+  scrollTo(_anchor: string) {
+    switch (_anchor) {
+      case 'cgu':
+        this.cguFragment.nativeElement.scrollIntoView({ behavior: "smooth", block: "start" });
+        break;
+      case 'presentationduservice':
+        this.presentationduserviceFragment.nativeElement.scrollIntoView({ behavior: "smooth", block: "start" });
+        break;
+      case 'modalites':
+        this.modalitesFragment.nativeElement.scrollIntoView({ behavior: "smooth", block: "start" });
+        break;
+      case 'traitementdesdonnees':
+        this.traitementdesdonneesFragment.nativeElement.scrollIntoView({ behavior: "smooth", block: "start" });
+        break;
+      case 'conservationdesdonnees':
+        this.conservationdesdonneesFragment.nativeElement.scrollIntoView({ behavior: "smooth", block: "start" });
+        break;
+      case 'engagementsetresponsabilite':
+        this.engagementsetresponsabiliteFragment.nativeElement.scrollIntoView({ behavior: "smooth", block: "start" });
+        break;
+      case 'qualiteduservice':
+        this.qualiteduserviceFragment.nativeElement.scrollIntoView({ behavior: "smooth", block: "start" });
+        break;
+      case 'securite':
+        this.securiteFragment.nativeElement.scrollIntoView({ behavior: "smooth", block: "start" });
+        break;
+      case 'validite':
+        this.validiteFragment.nativeElement.scrollIntoView({ behavior: "smooth", block: "start" });
+        break;
+    }
+  }
 }
--- src/app/components/politique-protection-donnees/politique-protection-donnees.component.ts
+++ src/app/components/politique-protection-donnees/politique-protection-donnees.component.ts
@@ -1,17 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { Title } from '@angular/platform-browser';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'ft-politique-protection-donnees',
   templateUrl: './politique-protection-donnees.component.html',
   styleUrls: ['./politique-protection-donnees.component.scss']
 })
-export class PolitiqueProtectionDonneesComponent implements OnInit {
+export class PolitiqueProtectionDonneesComponent implements OnInit, AfterViewInit {
 
-  constructor(private titleService: Title) { }
+  @ViewChild('politiquedeprotectiondesdonnees') private politiquedeprotectiondesdonneesFragment: ElementRef;
+  @ViewChild('droitsdespersonnes') private droitsdespersonnesFragment: ElementRef;
+
+  constructor(private titleService: Title, 
+    private router: Router) { }
 
   ngOnInit(): void {
     this.titleService.setTitle('France transfert - Politique de protection des données');
+  }
+
+  ngAfterViewInit(): void {
+    const tree = this.router.parseUrl(this.router.url);
+    if (tree.fragment) {
+      this.scrollTo(tree.fragment);
+    }
+  }
+
+  scrollTo(_anchor: string) {
+    switch (_anchor) {
+      case 'politiquedeprotectiondesdonnees':
+        this.politiquedeprotectiondesdonneesFragment.nativeElement.scrollIntoView({ behavior: "smooth", block: "start" });
+        break;
+      case 'droitsdespersonnes':
+        this.droitsdespersonnesFragment.nativeElement.scrollIntoView({ behavior: "smooth", block: "start" });
+        break;
+    }
   }
 
 }
