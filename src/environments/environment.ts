3c785bdddc55bff279ad92bec4710fb165d24bd9
Julien Aubert <j.aubert@actongroup.com>
2021-11-17 18:08:42 +0100

    error api mail

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,7 +4,7 @@
 
 export const environment = {
   production: false,
-  host: 'http://localhost:8081/',
+  host: 'https://test-francetransfert.aot.agency/',
   apis: {
     config: 'api-private/upload-module/config',
     upload: {
