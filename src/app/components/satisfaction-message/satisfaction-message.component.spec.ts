86aa13a7c76e1b55018b3c18b22b2c303fc05156
czouhri <c.zouhri@actongroup.com>
2022-01-04 11:22:14 +0100

    message de satisfaction

---
+++ src/app/components/satisfaction-message/satisfaction-message.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SatisfactionMessageComponent } from './satisfaction-message.component';
+
+describe('SatisfactionMessageComponent', () => {
+  let component: SatisfactionMessageComponent;
+  let fixture: ComponentFixture<SatisfactionMessageComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SatisfactionMessageComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SatisfactionMessageComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
