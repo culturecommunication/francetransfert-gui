87583073fff80962cdb1cf892d943ba14c7d727d
Julien Aubert <j.aubert@actongroup.com>
2022-07-25 18:31:48 +0200

    type import dest

--- src/app/components/mailing-list-manager/mailing-list-manager.component.ts
+++ src/app/components/mailing-list-manager/mailing-list-manager.component.ts
@@ -35,16 +35,16 @@ export class MailingListManagerComponent implements OnInit {
 
   loadMailingListFromFile(e) {
     this.file = e.target.files[0];
-    if(e.target.files[0].type.startsWith("text/")){
+    if (e.target.files[0].type.startsWith("text/") || (e.target.files[0].type == "application/vnd.ms-excel" && (e.target.files[0].name.endsWith(".csv") || e.target.files[0].name.endsWith(".txt")))) {
       let fileReader = new FileReader();
       fileReader.readAsText(this.file);
       fileReader.onload = (e) => {
         this.parseMailingListFile(fileReader.result);
       }
     }
-    else{
+    else {
       this.openSnackBar(4000);
-      }
+    }
 
   }
 
